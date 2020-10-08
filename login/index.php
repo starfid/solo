a0767b3cd9af7e778f89bc74805eaf2c5c509536
Starfid <50829388+starfid@users.noreply.github.com>
2020-10-08 19:26:58 +0700

    mobile view update

--- login/index.php
+++ login/index.php
@@ -9,8 +9,8 @@
 		<meta name="viewport" content="user-scalable=no, width=device-width" />
 		<meta name="viewport" content="minimum-scale=1.0,width=device-width,maximum-scale=1,user-scalable=no" />
 		<style>
-			body {background-color:grey;color:white;font-family:arial}
-			form {height:100%;position:relative}
+			body {background-color:grey;color:white;font-family:arial;}
+			form {height:100%;position:relative;}
 			table {margin: 0;position:absolute;top:50%;left:50%;-ms-transform:translate(-50%, -50%);transform:translate(-50%, -50%);}
 			h1 {margin:0;}
 			input,select{padding:4px;background-color:#353944;border:none;color:white;font-size:20px;-webkit-appearance: none;-moz-appearance: none;border-radius:none;border:none;outline:none}
@@ -20,7 +20,8 @@
 			select:active{border:none}
 			@media (max-width: 768px) {
 				#logo{display:none;}
-				table{width:100%;margin:0}
+				form {height:auto}
+				table{width:100%;margin:0;top:0;-ms-transform:translate(0,0);transform:translate(0,0);top:0;left:0}
 			}
 		</style>
 	</head>
