e4c4b14420563ec6487215bee13b8c38d0aca461
Starfid <50829388+starfid@users.noreply.github.com>
2022-07-08 19:18:37 +0700

    Add table and data sample

--- apps/admin/accounts/admin.php
+++ apps/admin/accounts/admin.php
@@ -15,6 +15,24 @@
 			return($this);
 		}
 		function search($arg){
+			
+			/*Remove this anytime*/
+			$this->sql[] = "create table if not exists user(
+				id int(9) not null auto_increment primary key,
+				entry datetime not null,
+				realname varchar(255) not null,
+				username varchar(255) not null,
+				pwd varchar(255) not null
+			)";
+			$this->sql[] = "insert into user(entry,realname,username,pwd)
+				select
+					now(),
+					'Administrator',
+					'admin',
+					'".md5('admin')."'
+				where not exists (select * from user)";
+			/*******************/
+			
 			$this->sql[] = "select
 					id as itemKey,
 					username as itemTitle,
