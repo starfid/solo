95b96976f5a789473ff5760c8dca79c9cc10c748
Starfid <50829388+starfid@users.noreply.github.com>
2022-07-08 16:35:03 +0700

    Add sample table and data

--- apps/guest/sample folder/example.php
+++ apps/guest/sample folder/example.php
@@ -1,8 +1,25 @@
 <?php
-	class Application implements Primary, Search, Compose, Update, Delete {
+	class Application implements Primary, Search  {
 		protected $sql;
 
 		function primary(){
+
+			/*Remove this anytime*/
+			$this->sql[] = "create table if not exists contact(
+				id int(9) not null auto_increment primary key,
+				name varchar(255) not null,
+				address varchar(255) not null
+			)";
+			$this->sql[] = "insert into contact(name,address)
+				select * from (
+					(select 'Steve Jobs', 'One Infinite Loop, Cupertino') union
+					(select 'Mark Zuckerberg', 'One Hacker Way, Menlo Park') union
+					(select 'Bill Gates', ' One Microsoft Way, Redmond') union
+					(select 'Elon Musk', 'One Tesla Road, Austin')
+				) as samples
+				where not exists (select * from contact)";
+			/*******************/
+
 			$this->sql[] = "select
 					id as itemKey,
 					name as itemTitle,
@@ -24,30 +41,6 @@
 				from contact
 				where name like '%".$arg."%' or address like '%".$arg."%'
 			";
-			return($this);
-		}
-		function compose($arg){
-			$this->sql[] = "insert into contact(
-					name,
-					address
-				) values(
-					'".$arg['name']."',
-					'".$arg['address']."'
-				)
-			";
-			return($this);
-		}
-		function update($arg){
-			$this->sql[] = "update contact set
-				name = '".$arg['name']."',
-				address = '".$arg['address']."'
-			where id = '".$arg['itemKey'][0]."'";
-			return($this);
-		}
-		function delete($arg){
-			foreach($arg['itemKey'] as $id){
-				$this->sql[] = "delete from contact where id = '".$id."'";
-			}
 			return($this);
 		}
 	}
