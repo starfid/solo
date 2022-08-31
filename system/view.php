4062ecb6ce02dbd247ab60dc9273e9de023535f6
Starfid <50829388+starfid@users.noreply.github.com>
2022-08-31 15:29:46 +0700

    nameFormat to group title

--- system/view.php
+++ system/view.php
@@ -45,7 +45,7 @@
 			}
 			$selIndex =	0;
 
-			$title = $this->nameFormat($this->current['app'])." ".ucwords($this->current['group']).", ".ucwords($this->meta['label']);
+			$title = $this->nameFormat($this->current['app'])." ".$this->nameFormat($this->current['group']).", ".ucwords($this->meta['label']);
 			$currentURL = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 			$searchAble = in_array('search',$this->current['methods']);
 			$disableSearch = $searchAble?'':'disabled ';
